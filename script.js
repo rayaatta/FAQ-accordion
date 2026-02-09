22e2da3519c999dee957a1f3c0853628a49263ff
rayaatta <95174248+rayaatta@users.noreply.github.com>
2026-02-09 20:24:08 +0300

    adding main files

---
+++ script.js
@@ -0,0 +1,11 @@
+/*const details = document.querySelector("details");
+const summary = document.querySelector("summary");
+const para = document.querySelector("details > p");
+details.addEventListener("click", () => {
+    if(summary.style.backgroundImage = "assets/images/icon-minus.svg";){
+        summary.style.backgroundImage = "assets/images/icon-plus.svg";
+    } else {
+    summary.style.backgroundImage = "assets/images/icon-minus.svg";
+    }
+    para.style.display = "block";
+})*/
